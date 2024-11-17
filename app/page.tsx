a7a306517cf3c8f369e618ff26efe2c814fa1bbb
hiroki <nadaqi@proton.me>
2024-11-17 11:16:36 +0900

    Add existing posts

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,31 @@ import { allPosts } from "@/.contentlayer/generated";
 import Link from "next/link";
 
 export default function Home() {
-	const posts = allPosts.sort((a, b) => (a.date > b.date ? -1 : 1));
+	allPosts.sort((a, b) => (a.date > b.date ? -1 : 1));
 
 	return (
-		<div className="prose dark:prose-invert">
-			{allPosts.map((post) => (
-				<article key={post._id}>
-					<Link href={post.slug}>
-						<h2>{post.title}</h2>
-					</Link>
-					{post.description && <p>{post.description}</p>}
-				</article>
-			))}
-		</div>
+		<>
+			<div className="flex items-center space-x-2 pt-10">
+				<img src="/logo.png" alt="logo" className="w-10 rounded-full" />
+				<h1 className="text-xl font-bold">hiroki</h1>
+				<Link
+					href="https://x.com/hirokiyn"
+					target="_blank"
+					className="text-slate-500 underline dark:text-slate-400"
+				>
+					@hirokiyn
+				</Link>
+			</div>
+			<div className="prose dark:prose-invert">
+				{allPosts.map((post) => (
+					<article key={post._id}>
+						<Link href={post.slug}>
+							<h2>{post.title}</h2>
+						</Link>
+						{post.description && <p>{post.description}</p>}
+					</article>
+				))}
+			</div>
+		</>
 	);
 }
